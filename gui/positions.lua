da39373f77c1327ad6e29b03f680eefead2e77c7
Eduardo Gomez Saldias <50159560+edugomez102@users.noreply.github.com>
2022-10-03 16:10:00 +0200

    update positions

--- gui/positions.lua
+++ gui/positions.lua
@@ -1,12 +1,21 @@
 local Card = require('uno.card')
 return {
-  main_card = {
+  current_card = {
     x = 568, y = 83,
     scale = 2
   },
   card_list = {
     x = 42, y = 537,
     margin = 8.27 + Card.w
+  },
+  player_list = {
+    x = 44, y = 391,
+    margin = 33.66
+  },
+  direction = {
+    x = 30, y = 336
+  },
+  gui_text = {
+    x = 139, y = 336
   }
-
 }
